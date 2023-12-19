e77e644f22192437e52896324697ba69c48bc9bc
Kevin Roan <kevinroan0303@gmail.com>
2023-12-19 16:19:58 +0530

    refactor: removed unused files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,4 @@
 export { default as AdminSideBar } from "./AdminSideBar";
 export { default as Card } from "./Card";
-export { default as PlanCard } from "./PlanCard";
 export { default as ToggleButton } from "./ToggleButton";
 export { default as Header } from "./Header";
